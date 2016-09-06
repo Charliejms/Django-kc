5a64093777122a4b9a76d1285f4088888aa6db49
Sr.Charlie <r.cacuango@gmail.com>
2016-09-06 21:12:28 +0200

    Primer modelo de Photos de Flickr

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Photo(models.Model):
+
+    COPYRIGTH = 'RIG'
+    COPYLEFT = 'LEF'
+    CREATIVE_COMMONS = 'CC'
+
+    LICENSES = (
+        (COPYRIGTH, 'Copyrigth'),
+        (COPYLEFT, 'Copyleft'),
+        (CREATIVE_COMMONS, 'Creative Commons')
+    )
+
+    name = models.CharField(max_length=150)
+    url = models.URLField()
+    description = models.TextField()
+    license = models.CharField(max_length=3, choices=LICENSES)
+    create_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_created=True)
